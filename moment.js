6723c5473d4ac9a2a99473a7b4a388b4580df801
v-samoylov <samoylov.vladimir@yahoo.com>
2015-11-16 03:40:48 +0500

    Bux fix 2

--- moment.js
+++ moment.js
@@ -7,11 +7,16 @@ module.exports = function () {
         timezone: null,
 
         format: function (pattern) {
+            if (!this.timezone) {
+                this.timezone = -new Date().getTimezoneOffset() / 60;
+            }
             var dates = {1: 'ПН', 2: 'ВТ', 3: 'СР'};
             var day = dates[this.date.getDate()];
             pattern = pattern.replace('%DD', day);
             pattern = pattern.replace('%HH', this.date.getHours() + this.timezone);
-            pattern = pattern.replace('%MM', this.date.getMinutes());
+            var minutes = this.date.getMinutes();
+            minutes = minutes < 10 ? '0' + minutes.toString() : minutes;
+            pattern = pattern.replace('%MM', minutes);
             return pattern;
         },
 
@@ -21,6 +26,7 @@ module.exports = function () {
             var days = Math.floor(time / 60 / 24);
             var hours = Math.floor(time / 60 % 24);
             var minutes = Math.floor(time - days * 1440 - hours * 60);
+            minutes = minutes < 10 ? '0' + minutes.toString() : minutes;
             pattern = pattern.replace('%DD', days);
             pattern = pattern.replace('%HH', hours);
             pattern = pattern.replace('%MM', minutes);
